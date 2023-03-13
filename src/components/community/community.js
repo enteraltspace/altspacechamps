e9ccf02b348f799b63925f36cca0368a39bc47eb
enteraltspace <119533151+enteraltspace@users.noreply.github.com>
2023-03-13 14:23:15 +0530

    Update community.js

--- src/components/community/community.js
+++ src/components/community/community.js
@@ -1,18 +1,17 @@
-import React from "react";
-import "./community.css";
-function Community() {
-  return (
-    <>
-      <div className="community-div1">
-        <h1 className="community-text">JOIN OUR COMMUNITY</h1>
-        <a href=" https://discord.gg/ru6g942RKe" target="_blank">
-          <button class="community-button">JOIN DISCORD</button>
-        </a>
-      </div>
-      <div className="community-div1">
-        <img src={require("./circles.png")} className="community-img1" />
-      </div>
-    </>
-  );
+import React from"react"
+import "./community.css"
+function Community(){
+    return(<>
+    <div className="community-div1">
+        <h2 className="community-heading">
+            Tokenomics
+        </h2>
+        <a href="#"><button className="community-button">Start Minting</button></a>
+        <img src={require("./tokenomics.png")} className="community-img1"/>
+    </div>
+    <div className="community-div1">
+        <img src={require("./circles.png")} className="community-img1"/>
+    </div>
+    </>)
 }
-export default Community;
+export default Community
