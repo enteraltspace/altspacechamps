14d76938775684d0dc9f5734b1edd55e4f14027d
enteraltspace <119533151+enteraltspace@users.noreply.github.com>
2023-03-14 18:41:01 +0530

    Update welcome.js

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -5,15 +5,22 @@ function Welcome() {
   return (
     <>
       <div className="welcome-div1" id="home">
-        <h1 className="welcome-heading">Enter ALT Space</h1>
-        <h1 className="welcome-heading1">as Champs</h1>
-        <h4 className="welcome-text">
-          ALT Space Champs collection is made up of 10,000 randomly generated
-          NFTs that exist on the Polygon blockchain. The holders of ALT Space
-          Champs NFTs can participate in an exclusive forum that brings together
-          Web3 builders under 1 roof. Keep in mind that all Champs have caps,
-          and that's what makes them different from everyone else in ALT Space.
-        </h4>
+        <h1 className="welcome-heading">Enter ALT Space </h1>
+        <h2 className="welcome-heading1">as Champs</h2>
+        <div className="welcome-text">
+          <p>
+            ALT Space Champs collection is made up of 9,999 randomly generated
+            NFTs that exist on the Polygon blockchain. The holders of Champs
+            NFTs can participate in an exclusive token-gated lounge for tech and Web3 enthusiasts.
+          </p>
+
+          <p>
+            Timeline: We are having our <strong>private sale</strong> now with
+            reduced pricing. The <strong>public launch</strong> will be on Mar
+            23, 2023.
+          </p>
+        </div>
+        <br />
       </div>
       <div className="welcome-div2">
         <Mint />
