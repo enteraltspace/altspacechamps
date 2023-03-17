8a614615277f5d661430f069306cab1122542d77
enteraltspace <119533151+enteraltspace@users.noreply.github.com>
2023-03-17 14:14:37 +0530

    Update roadmap.js

--- src/components/roadmap/roadmap.js
+++ src/components/roadmap/roadmap.js
@@ -1,49 +1,45 @@
 import React from "react";
-import './roadmap.css'
-function Roadmap(){
-    return(<>
-    <h2 className="roadmap-heading" id="roadmap">
+import "./roadmap.css";
+function Roadmap() {
+  return (
+    <>
+      <h1 className="roadmap-heading" id="roadmap">
         ROADMAP
-    </h2>
-    <div className="main-div">
-    <div className="circular-div">
-<h3>1</h3>
-</div>
-<div className="rectangular-div">
-<div className="roadmap-text"><strong>PHASE 1 (Prelaunch) - Completed</strong></div>
-<div className="roadmap-text">✓ Website Development</div>
-<div className="roadmap-text">✓ Create NFT Collection</div>
-<div className="roadmap-text">✓ Partnership with Global Events</div>
-<div className="roadmap-text">✓ Create Board of Advisors</div>
-<div className="roadmap-text">✓ Private Sale Open - 1000 NFTs</div>          
-</div>
-</div>
-<div className="main-div">
-<div className="circular-div">
-           
-<h3>2</h3>
-</div>
-<div className="rectangular-div">
-<div className="roadmap-text"><strong>PHASE 2 (Launch) - March 2023</strong></div>
-<div className="roadmap-text">Launch of Token-Gated Lounge</div>
-<div className="roadmap-text">Launch of Elevator Reels</div>
-<div className="roadmap-text">Launch the Wall of Fame</div>
-</div>
-    </div>
-    <div className="main-div">
-    <div className="circular-div">
-           
-<h3>3</h3>
-</div>
-<div className="rectangular-div">
-<div className="roadmap-text"><strong>PHASE 3 - June 2023</strong></div>
-<div className="roadmap-text">NFT Staking</div>
-<div className="roadmap-text">Airdrop NFTs of New Collections</div>
-<div className="roadmap-text">New Utilities for NFT holders</div>
-<div className="roadmap-text">Organize Virtual Summits</div>
-</div>
-    </div>
-    
-    </>)
+      </h1>
+      <div className="main-div">
+        <div className="circular-div">
+          <h1>1</h1>
+        </div>
+        <div className="rectangular-div">
+          <p className="roadmap-text">Website Development</p>
+          <p className="roadmap-text">Create NFT Collection</p>
+          <p className="roadmap-text">Private Sale Open</p>
+          <p className="roadmap-text">Mint NFTs</p>
+          <p className="roadmap-text">Build Community</p>
+        </div>
+      </div>
+      <div className="main-div">
+        <div className="circular-div">
+          <h1>2</h1>
+        </div>
+        <div className="rectangular-div">
+          <p className="roadmap-text">Marketplace Listings</p>
+          <p className="roadmap-text">Airdrop NFTs of New Collections</p>
+          <p className="roadmap-text">Exclusive Portal for Collaboration</p>
+        </div>
+      </div>
+      <div className="main-div">
+        <div className="circular-div">
+          <h1>3</h1>
+        </div>
+        <div className="rectangular-div">
+          <p className="roadmap-text">NFT Staking</p>
+          <p className="roadmap-text">NFT Passive incomes</p>
+          <p className="roadmap-text">New Utilities for NFT holders</p>
+          <p className="roadmap-text">Interact in Metaverse</p>
+        </div>
+      </div>
+    </>
+  );
 }
-export default Roadmap
+export default Roadmap;
