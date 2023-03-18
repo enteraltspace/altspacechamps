e25de9dec982fe04b68807ffae8c175e12cc15ae
enteraltspace <119533151+enteraltspace@users.noreply.github.com>
2023-03-18 07:47:13 +0530

    Update welcome.js

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -6,7 +6,7 @@ function Welcome() {
     <>
       <div className="welcome-div1" id="home">
         <h1 className="welcome-heading">Enter ALT Space </h1>
-        <h2 className="welcome-heading1">as Champs</h2>
+        <h2 className="welcome-heading1"><p>as Champs</p></h2>
         <div className="welcome-text">
           <p>
             ALT Space Champs collection is made up of 9,999 randomly generated
