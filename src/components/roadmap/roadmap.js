7ce0e9c87348d171c4e8c3e48c898b74f7c1b85b
enteraltspace <119533151+enteraltspace@users.noreply.github.com>
2023-04-09 15:22:22 +0530

    Update roadmap.js

--- src/components/roadmap/roadmap.js
+++ src/components/roadmap/roadmap.js
@@ -11,7 +11,7 @@ function Roadmap() {
           <h2>1</h2>
         </div>
         <div className="rectangular-div">
-          <p className="roadmap-text"><strong>PHASE 1 (Prelaunch) - Completed</strong></p>
+          <p className="roadmap-text"><strong>PHASE 1 - Completed in Q1 2023</strong></p>
           <p className="roadmap-text">✓ Website Development</p>
           <p className="roadmap-text">✓ Create NFT Collection</p>
           <p className="roadmap-text">✓ Private Sale Open</p>
@@ -24,10 +24,10 @@ function Roadmap() {
           <h2>2</h2>
         </div>
         <div className="rectangular-div">
-          <p className="roadmap-text"><strong>PHASE 2 (Launch) - March 2023</strong></p>
-          <p className="roadmap-text">Marketplace Listings</p>
-          <p className="roadmap-text">Airdrop NFTs of New Collections</p>
+          <p className="roadmap-text"><strong>PHASE 2 - Q2 2023</strong></p>
           <p className="roadmap-text">Exclusive Portal for Collaboration</p>
+          <p className="roadmap-text">Free Mint NFTs of New Collections</p>
+          <p className="roadmap-text"></p>
         </div>
       </div>
       <div className="main-div">
@@ -35,7 +35,7 @@ function Roadmap() {
           <h2>3</h2>
         </div>
         <div className="rectangular-div">
-          <p className="roadmap-text"><strong>PHASE 3 - June 2023</strong></p>
+          <p className="roadmap-text"><strong>PHASE 3 - Q3 2023</strong></p>
           <p className="roadmap-text">NFT Staking</p>
           <p className="roadmap-text">NFT Passive incomes</p>
           <p className="roadmap-text">New Utilities for NFT holders</p>
